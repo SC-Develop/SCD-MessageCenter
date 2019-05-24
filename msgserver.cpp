af0f930f1fbf765ae39f07aca4146d9fb97e0181
Salvatore Cerami <git.sc.develop@gmail.com>
2019-05-24 16:15:58 +0200

    Update msgserver.cpp

--- msgserver.cpp
+++ msgserver.cpp
@@ -1,5 +1,5 @@
 /**
- * @class  SCDMsgServer - https://github.com/sc-develop/SCD_MC
+ * @class  SCDMsgServer - https://github.com/sc-develop/SCD-MC
  *
  * @author Ing. Salvatore Cerami - dev.salvatore.cerami@gmail.com - https://github.com/sc-develop/
  *
